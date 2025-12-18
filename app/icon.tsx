428f59600307057ef1f604e0b42f578db7a89a54
Dylan Wahbe <dylan@wahbe.com>
2025-12-17 22:01:12 -0800

    New favicon

--- app/icon.tsx
+++ app/icon.tsx
@@ -16,34 +16,13 @@ export default function Icon() {
         alignItems: 'center',
         justifyContent: 'center',
         background: '#fff2e2',
-        borderRadius: '50%',
+        borderRadius: 9999,
+        border: '2px solid #f59e0b',
+        fontSize: 20,
+        lineHeight: 1,
       }}
     >
-      <svg
-        width="28"
-        height="28"
-        viewBox="0 0 32 32"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        {/* Soil/pot base */}
-        <ellipse cx="16" cy="24" rx="7" ry="3" fill="#d97706" />
-
-        {/* Stem */}
-        <path
-          d="M16 24 Q16 18 16 14"
-          stroke="#65a30d"
-          strokeWidth="2.5"
-          strokeLinecap="round"
-          fill="none"
-        />
-
-        {/* Left leaf */}
-        <path d="M16 16 Q12 14 10 10 Q14 11 16 16" fill="#84cc16" />
-
-        {/* Right leaf */}
-        <path d="M16 14 Q20 11 22 7 Q18 10 16 14" fill="#65a30d" />
-      </svg>
+      🌱
     </div>,
     {
       ...size,
